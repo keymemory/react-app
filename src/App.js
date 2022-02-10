4575ffb4b1602cd9f18ff71c74508ee0cc246575
keymemory <keymemory@naver.com>
2022-02-10 16:59:23 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import React, { Component } from "react";
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TOC from './components/TOC';
+import Content from './components/Content';
+import Subject from './components/Subject';
 import './App.css';
 
 
-class App extends Component {
+// state, props의 값이 변경되면 render()가 자동 실행
+class App extends Component { // 초기화하고 싶은 코드는 constructor 에 작성
+  constructor(props) { // constructor - state 사용을 위해 작성 / 가장 먼저 실행되는 함수
+    super(props);
+    this.state = {
+      mode: 'welcome',
+      subject: { title: 'WEB', sub: 'World Wide Web!' },
+      welcome: { title: 'Welcome', desc: 'Hello, React!!' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is for information' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' }
+      ]
+    }
+  }
+
+
   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+
     return (
       <div className="App">
-        Hello, React!!
+        {/*<Subject
+          title={this.state.subject.title} // {} 중괄호로 묶으면 자바스크립트 코드로 취급
+        sub={this.state.subject.sub}></Subject>*/}
+        <header>
+          <h1><a href="/" onClick={function(e) {
+            console.log(e);
+            e.preventDefault();
+          }}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
@@ -16,57 +55,3 @@ class App extends Component {
 
 export default App;
 
-
-// class App extends Component{
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-Header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
-
-
-
-
-
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
